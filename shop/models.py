a969c29bdcd46ef11aec50272a948a748f9c173b
oseeshogun <omasuaku@gmail.com>
2025-01-17 22:50:54 +0100

    feat: Product & Category models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+    price = models.IntegerField(validators=[
+        MinValueValidator(100)
+    ])
+    image_url = models.CharField(max_length=200)
+    rate = models.IntegerField(validators=[
+        MaxValueValidator(5),
+        MinValueValidator(1)
+    ])
+    description = models.TextField()
+    popularity = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['popularity']
+
+
+class MoreProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=200)
