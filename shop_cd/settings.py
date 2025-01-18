3ce2aa75d637055e527c8b986e02299d79a9f736
oseeshogun <omasuaku@gmail.com>
2025-01-18 12:14:07 +0100

    MEDIA_ROOT for docker

--- shop_cd/settings.py
+++ shop_cd/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -161,7 +161,7 @@ IMAGEKIT_CACHEFILE_DIR = 'CACHE/images'
 
 # Media settings for handling images
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', BASE_DIR / 'mediafiles')
 
 GRAPH_MODELS = {
   'all_applications': True,
